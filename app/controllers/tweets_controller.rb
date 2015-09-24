be22919b64f318cbad6965a14c74ed9fdebfac11
sswbelser <sswbelser@gmail.com>
2015-09-24 15:23:36 -0700

    Major UX changes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,6 +7,7 @@ class TweetsController < ApplicationController
     current_user.tweet(twitter_params[:message])
   end
 
+  # sets the parameters for tweets
   def twitter_params
     params.require(:tweet).permit(:message)
   end
