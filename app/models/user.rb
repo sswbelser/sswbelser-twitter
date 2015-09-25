e00244a13d2941e9ac5df02888b34d47b5ba77b5
sswbelser <sswbelser@gmail.com>
2015-09-24 20:17:11 -0700

    Added user_timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,4 +34,15 @@ class User < ActiveRecord::Base
     return client.home_timeline
   end
 
+  # grabs Twitter user's user_timeline
+  def user_timeline
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.config.twitter_key
+      config.consumer_secret     = Rails.application.config.twitter_secret
+      config.access_token        = oauth_token
+      config.access_token_secret = oauth_secret
+    end
+    return client.user_timeline
+  end
+
 end
